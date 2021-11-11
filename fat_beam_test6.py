fat beam calculation fixed, works well

fat_beam_test6.py
@@ -3,21 +3,23 @@ import hibplib as hb
 import hibpplotlib as hbplot
 import copy
 import matplotlib.pyplot as plt
+import time
 
 # %%
 ''' test FAT beam with focusing
 '''
-Ebeam = 140.
-UA2 = 3.0
+Ebeam = 132.
+UA2 = 2.0
 
-n_slits = 7
+# n_slits = 5
 # add slits to Geometry
-# geomT15.add_slits(n_slits=n_slits, slit_dist=0.01, slit_w=5e-3,
-#                   slit_l=0.1, slit_gamma=-20.)
-r_slits = geomT15.slits_edges
-rs = geomT15.r_dict['slit']
+# geomTJ2.add_slits(n_slits=n_slits, slit_dist=0.012, slit_w=4e-3,
+#                   slit_l=0.1, slit_gamma=20.)
+slits = range(5)  # [2]
+r_slits = geomTJ2.plates_dict['an'].slits_edges
+rs = geomTJ2.r_dict['slit']
 # calculate normal to slit plane
-slit_plane_n = geomT15.slit_plane_n
+slit_plane_n = geomTJ2.plates_dict['an'].slit_plane_n
 
 # %%
 traj_list_copy = copy.deepcopy(traj_list_a3b3)
@@ -25,18 +27,19 @@ traj_list_copy = copy.deepcopy(traj_list_a3b3)
 
 # %%
 # set number of filaments in a beam
-d_beam = 0.02  # beam diameter [m]
-n_filaments_xy = 5  # number of filaments in xy plane (must be ODD)
+d_beam = 0.01  # beam diameter [m]
+n_filaments_xy = 3  # number of filaments in xy plane (must be ODD)
 skip_center_traj = True
 n_gamma = 4  # number of chords in beam cross-section
-foc_len = 50  # distance from the first point of the trajectory to the focus
+foc_len = 50.  # distance from the first point of the trajectory to the focus
 drad = np.pi/180.  # converts degrees to radians
 
 fat_beam_list = []
 
+t1 = time.time()
 for tr in traj_list_copy:
-    if tr.Ebeam == Ebeam and tr.U[0] == UA2:
-        print('\nEb = {}, UA2 = {}'.format(tr.Ebeam, tr.U[0]))
+    if tr.Ebeam == Ebeam and abs(tr.U['A2'] - UA2) < 0.1:
+        print('\nEb = {}, UA2 = {}'.format(tr.Ebeam, tr.U['A2']))
     else:
         continue
     r0 = tr.RV0[0, :3]
@@ -59,7 +62,7 @@ for tr in traj_list_copy:
         # for gamma in np.arange(np.pi/n_gamma, np.pi, np.pi/n_gamma):
         for gamma in np.arange(0, np.pi, np.pi/n_gamma):
             gamma = gamma/drad
-            print('gamma = ', gamma)
+            print('\n gamma = ', gamma)
             # rotate and translate r0 to beam starting point
             r_rot = hb.rotate(r, axis=(1, 0, 0), deg=gamma)
             r_rot = hb.rotate(r_rot, axis=(0, 0, 1), deg=tr.alpha)
@@ -71,13 +74,25 @@ for tr in traj_list_copy:
 
             tr_fat = copy.deepcopy(tr)
             tr_fat.RV0[0, :] = np.hstack([r_rot, v_rot])
-            # tr_fat.U = [0., 0., 0., 0.]
-            # tr_fat.pass_prim(E, B, geomT15, tmax=0.01)
-            tr_fat = hb.pass_to_slits(tr_fat, dt, E, B, geomT15,
-                                      timestep_divider=10)
+            tr_fat.pass_fan(geomTJ2.r_dict['aim'], E, B, geomTJ2,
+                            eps_xy=1e-3, eps_z=1,
+                            stop_plane_n=np.array([1., 0., 0.]),
+                            no_intersect=True, no_out_of_bounds=True)
+            tr_fat.pass_to_target(geomTJ2.r_dict['aim'], E, B, geomTJ2,
+                                  eps_xy=1e-3, eps_z=1,
+                                  stop_plane_n=np.array([1., 0., 0.]),
+                                  no_intersect=True, no_out_of_bounds=True)
+            # tr_fat = hb.pass_to_slits(tr_fat, dt, E, B, geomTJ2,
+            #                           timestep_divider=10)
+            tr_fat = hb.calc_zones(tr_fat, dt, E, B, geomTJ2, slits=slits,
+                                   timestep_divider=6,
+                                   eps_xy=1e-4, eps_z=1, dt_min=1e-13,
+                                   no_intersect=True, no_out_of_bounds=True)
             fat_beam_list.append(tr_fat)
             if abs(y) < 1e-6:
                 break
+t2 = time.time()
+print('t = {:.1f} s\n'.format(t2-t1))
 
 # %% save zones
 # dirname = 'output/' + 'B{}_I{}'.format(int(Btor), int(Ipl))
@@ -90,13 +105,13 @@ for tr in traj_list_copy:
 #     np.savetxt(fname, zone, fmt='%.4e')
 
 # %% plot fat beam
-hbplot.plot_fat_beam(fat_beam_list, geomT15, Btor, Ipl, n_slit='all')
-hbplot.plot_fat_beam(fat_beam_list, geomT15, Btor, Ipl, n_slit=2)
+hbplot.plot_fat_beam(fat_beam_list, geomTJ2, config, slits=range(5))
+hbplot.plot_fat_beam(fat_beam_list, geomTJ2, config, slits=slits)
 
 # %% plot SVs
-hbplot.plot_svs(fat_beam_list, geomT15, Btor, Ipl, n_slit='all',
+hbplot.plot_svs(fat_beam_list, geomTJ2, config, slits=range(5),
                 plot_prim=True, plot_sec=False, plot_zones=False,
                 plot_cut=False, alpha_xy=10, alpha_zy=20)
-hbplot.plot_svs(fat_beam_list, geomT15, Btor, Ipl, n_slit=2,
+hbplot.plot_svs(fat_beam_list, geomTJ2, config, slits=slits,
                 plot_prim=True, plot_sec=False, plot_zones=True,
                 plot_cut=False, alpha_xy=10, alpha_zy=20)

hibplib.py
@@ -54,7 +54,7 @@ class Traj():
         self.Ebeam = Ebeam
         # particle velocity:
         Vabs = np.sqrt(2 * Ebeam * 1.602176634E-16 / m)
-        V0 = calc_vector(Vabs, alpha, beta, direction=(-1, -1, 1))
+        V0 = calc_vector(Vabs, alpha, beta, direction=(-1., -1., 1.))
         self.alpha = alpha
         self.beta = beta
         self.U = U
@@ -140,7 +140,7 @@ class Traj():
         self.tag_prim = tag_column
 
     def pass_sec(self, RV0, r_aim, E_interp, B_interp, geom,
-                 stop_plane_n=np.array([1, 0, 0]), tmax=5e-5,
+                 stop_plane_n=np.array([1., 0., 0.]), tmax=5e-5,
                  eps_xy=1e-3, eps_z=1e-3):
         '''
         passing secondary trajectory from initial point RV0 to point r_aim
@@ -220,7 +220,7 @@ class Traj():
         self.tag_sec = tag_column
 
     def pass_fan(self, r_aim, E_interp, B_interp, geom,
-                 stop_plane_n=np.array([1, 0, 0]), eps_xy=1e-3, eps_z=1e-3,
+                 stop_plane_n=np.array([1., 0., 0]), eps_xy=1e-3, eps_z=1e-3,
                  no_intersect=False, no_out_of_bounds=False):
         '''
         passing fan from initial point self.RV0
@@ -257,7 +257,7 @@ class Traj():
         self.Fan = list_sec
 
     def pass_to_target(self, r_aim, E_interp, B_interp, geom,
-                       stop_plane_n=np.array([1, 0, 0]),
+                       stop_plane_n=np.array([1., 0., 0.]),
                        eps_xy=1e-3, eps_z=1e-3, dt_min=1e-10,
                        no_intersect=False, no_out_of_bounds=False):
         '''
@@ -780,7 +780,7 @@ def add_diafragm(geom, plts_name, diaf_name, diaf_width=0.1):
 
 # %%
 @numba.njit()
-def calc_vector(length, alpha, beta, direction=(1, 1, -1)):
+def calc_vector(length, alpha, beta, direction=(1., 1., -1.)):
     '''
     calculate vector based on its length and angles
     '''
@@ -966,7 +966,7 @@ def optimize_A3B3(tr, geom, UA3, UB3, dUA3, dUB3,
         stop_plane_n = geom.plates_dict['an'].det_plane_n
     elif target == 'A4':
         rs = geom.r_dict['A4']
-        stop_plane_n = calc_vector(1.0, 0., 0., direction=(1, 1, 1))
+        stop_plane_n = calc_vector(1.0, 0., 0., direction=(1., 1., 1.))
 
     tr.dt1 = dt
     tr.dt2 = dt
@@ -1094,7 +1094,7 @@ def optimize_A4(tr, geom, UA4, dUA4, E, B, dt, eps_alpha=0.1):
 
 # %%
 def calc_zones(tr, dt, E, B, geom, slits=[2], timestep_divider=5,
-               stop_plane_n=np.array([1, 0, 0]), eps_xy=1e-3, eps_z=1,
+               stop_plane_n=np.array([1., 0., 0.]), eps_xy=1e-3, eps_z=1,
                dt_min=1e-11, no_intersect=True, no_out_of_bounds=True):
     '''
     calculate ionization zones
@@ -1152,7 +1152,7 @@ def calc_zones(tr, dt, E, B, geom, slits=[2], timestep_divider=5,
         if not (True in tr.IntersectGeometrySec.values() or
                 tr.B_out_of_bounds) and contains_point:
             fan_list.append(tr.RV_sec)
-    tr.Fan = fan_list
+    # tr.Fan = fan_list
     tr.fan_to_slits = fan_list
     print('\nPrecise fan calculated')
 
@@ -1166,8 +1166,9 @@ def calc_zones(tr, dt, E, B, geom, slits=[2], timestep_divider=5,
         rv_list = []  # list for RV arrays of secondaries
         for edge in [upper_edge, lower_edge]:
             # find intersection of fan and slit edge
-            for i_tr in range(len(tr.Fan) - 1):
-                p1, p2 = tr.Fan[i_tr][-1, :3], tr.Fan[i_tr+1][-1, :3]
+            for i_tr in range(len(tr.fan_to_slits) - 1):
+                p1 = tr.fan_to_slits[i_tr][-1, :3]
+                p2 = tr.fan_to_slits[i_tr+1][-1, :3]
                 # check intersection between fan segment and slit edge
                 if is_intersect(p1, p2, edge[0], edge[1]):
                     r_intersect = segm_intersect(p1, p2, edge[0], edge[1])
@@ -1181,7 +1182,7 @@ def calc_zones(tr, dt, E, B, geom, slits=[2], timestep_divider=5,
                                       stop_plane_n=slit_plane_n,
                                       no_intersect=no_intersect,
                                       no_out_of_bounds=no_out_of_bounds)
-                    zones_list.append(tr.RV_sec[-1, :3])
+                    zones_list.append(tr.RV_sec[0, :3])
                     rv_list.append(tr.RV_sec)
                     print('ok!')
                     break
@@ -1405,11 +1406,11 @@ def is_between(A, B, C, eps=1e-6):
 
 @numba.njit()
 def order(A, B, C):
-        '''
-        if counterclockwise return True
-        if clockwise return False
-        '''
-        return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+    '''
+    if counterclockwise return True
+    if clockwise return False
+    '''
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
 
 
 @numba.njit()

hibpplotlib.py
@@ -828,7 +828,8 @@ def plot_grid_a3b3(traj_list, geom, config,
 
 
 # %%
-def plot_traj_toslits(tr, geom, config, slits=[2], plot_fan=True):
+def plot_traj_toslits(traj_list, geom, config, Ebeam=132.,
+                      slits=[2], plot_fan=True):
     '''
     plot fan of trajectories which go to slits
     '''
@@ -838,8 +839,7 @@ def plot_traj_toslits(tr, geom, config, slits=[2], plot_fan=True):
     set_axes_param(ax1, 'X (m)', 'Y (m)')
     set_axes_param(ax2, 'X (m)', 'Z (m)')
     set_axes_param(ax3, 'Z (m)', 'Y (m)')
-    ax1.set_title('E={} keV, UA2={} kV, {}'
-                  .format(tr.Ebeam, tr.U['A2'], config))
+    ax1.set_title('E={} keV, {}'.format(Ebeam,  config))
 
     # plot geometry
     geom.plot(ax1, axes='XY', plot_aim=False, plot_analyzer=True)
@@ -853,39 +853,39 @@ def plot_traj_toslits(tr, geom, config, slits=[2], plot_fan=True):
     colors = colors[:n_slits]
     colors = cycle(colors)
 
-    # plot primary trajectory
-    tr.plot_prim(ax1, axes='XY', color='k', full_primary=True)
-    tr.plot_prim(ax2, axes='XZ', color='k', full_primary=True)
-    tr.plot_prim(ax3, axes='ZY', color='k', full_primary=True)
-
-    # plot precise fan of secondaries
-    if plot_fan:
-        for fan_tr in tr.fan_to_slits:
-            ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color='tab:gray')
-            ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color='tab:gray')
-            ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color='tab:gray')
-
-    # plot secondaries
-    for i_slit in slits:
-        c = next(colors)
-        for fan_tr in tr.RV_sec_toslits[i_slit]:
-            ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color=c)
-            ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color=c)
-            ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color=c)
-
-    # plot zones
-    for i_slit in slits:
-        c = next(colors)
-        for fan_tr in tr.RV_sec_toslits[i_slit]:
-            ax1.plot(fan_tr[0, 0], fan_tr[0, 1], 'o', color=c,
-                     markerfacecolor='white')
-            ax3.plot(fan_tr[0, 2], fan_tr[0, 1], 'o', color=c,
-                     markerfacecolor='white')
+    for tr in traj_list:
+        if abs(tr.Ebeam - Ebeam) > 0.1:
+            continue
+        # plot primary trajectory
+        tr.plot_prim(ax1, axes='XY', color='k', full_primary=True)
+        tr.plot_prim(ax2, axes='XZ', color='k', full_primary=True)
+        tr.plot_prim(ax3, axes='ZY', color='k', full_primary=True)
+        # plot precise fan of secondaries
+        if plot_fan:
+            for fan_tr in tr.fan_to_slits:
+                ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color='tab:gray')
+                ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color='tab:gray')
+                ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color='tab:gray')
+        # plot secondaries
+        for i_slit in slits:
+            c = next(colors)
+            for fan_tr in tr.RV_sec_toslits[i_slit]:
+                ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color=c)
+                ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color=c)
+                ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color=c)
+        # plot zones
+        for i_slit in slits:
+            c = next(colors)
+            for fan_tr in tr.RV_sec_toslits[i_slit]:
+                ax1.plot(fan_tr[0, 0], fan_tr[0, 1], 'o', color=c,
+                         markerfacecolor='white')
+                ax3.plot(fan_tr[0, 2], fan_tr[0, 1], 'o', color=c,
+                         markerfacecolor='white')
     plt.show()
 
 
 # %%
-def plot_fat_beam(fat_beam_list, geom, config, n_slit='all', scale=3):
+def plot_fat_beam(fat_beam_list, geom, config, slits=[2], scale=3):
 
     # fig, (ax1, ax3) = plt.subplots(nrows=1, ncols=2)
     fig, axs = plt.subplots(2, 2, sharex='col', #sharey='row',
@@ -905,16 +905,11 @@ def plot_fat_beam(fat_beam_list, geom, config, n_slit='all', scale=3):
     geom.plot(ax3, axes='ZY', plot_aim=False, plot_analyzer=True)
 
     # get number of slits
-    n_slits = geom.slits_edges.shape[0]
+    n_slits = geom.plates_dict['an'].slits_edges.shape[0]
     # set color cycler
     prop_cycle = plt.rcParams['axes.prop_cycle']
     colors = prop_cycle.by_key()['color']
     colors = colors[:n_slits]
-
-    if n_slit == 'all':
-        slits = range(n_slits)
-    else:
-        slits = [n_slit]
 
     # plot trajectories
     for tr in fat_beam_list:
@@ -933,6 +928,8 @@ def plot_fat_beam(fat_beam_list, geom, config, n_slit='all', scale=3):
         # plot secondaries
         for i in slits:
             c = colors[i]
+            if tr.RV_sec_toslits[i] == None or len(tr.RV_sec_toslits[i]) == 0:
+                continue
             for fan_tr in tr.RV_sec_toslits[i]:
                 ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color=c)
                 ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color=c)
@@ -949,7 +946,7 @@ def plot_fat_beam(fat_beam_list, geom, config, n_slit='all', scale=3):
 
 
 # %%
-def plot_svs(fat_beam_list, geom, config, n_slit='all',
+def plot_svs(fat_beam_list, geom, config, slits=[2],
              plot_prim=True, plot_sec=False, plot_zones=True, plot_cut=False,
              plot_sep=True, alpha_xy=10, alpha_zy=20):
     '''
@@ -969,7 +966,7 @@ def plot_svs(fat_beam_list, geom, config, n_slit='all',
     geom.plot(ax3, axes='ZY', plot_aim=False, plot_analyzer=True)
 
     # get number of slits
-    n_slits = geom.slits_edges.shape[0]
+    n_slits = geom.plates_dict['an'].slits_edges.shape[0]
     # set color cycler
     prop_cycle = plt.rcParams['axes.prop_cycle']
     colors = prop_cycle.by_key()['color']
@@ -989,11 +986,6 @@ def plot_svs(fat_beam_list, geom, config, n_slit='all',
                     ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color=c)
                     ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color=c)
 
-    if n_slit == 'all':
-        slits = reversed(range(n_slits))
-    else:
-        slits = [n_slit]
-
     # plot sample volumes
     for i in slits:
         c = colors[i]
@@ -1002,7 +994,7 @@ def plot_svs(fat_beam_list, geom, config, n_slit='all',
         coords_last = np.empty([0, 3])
         for tr in fat_beam_list:
             # skip empty arrays
-            if tr.ion_zones[i].shape[0] == 0:
+            if tr.ion_zones[i] is None:
                 continue
             coords_first = np.vstack([coords_first, tr.ion_zones[i][0, 0:3]])
             coords_last = np.vstack([coords_last, tr.ion_zones[i][-1, 0:3]])

import_TS_profile.py
@@ -93,8 +93,8 @@ def ImportTS(shot, t_TS, neAvg, TeFit, NeFit,
 
     # %% make fitting of TS data
     # fitting of Te
-    rho_min = -0.5
-    rho_max = 0.3
+    rho_min = -0.6
+    rho_max = 0.1
     mask_Te = (Te[:, 0] > rho_min) & (Te[:, 0] < rho_max)
     Te_data = Te[mask_Te]
 
@@ -104,8 +104,10 @@ def ImportTS(shot, t_TS, neAvg, TeFit, NeFit,
 #    NeErr = NeErr[mask]
 
     # add boundary zeros
-    Te_data = np.vstack(([[-1, 0.04], [-0.8, 0.06]]*500, Te_data))
-    Te_data = np.vstack((Te_data, [[0.8, 0.06], [1, 0.04]]*500))
+    Te_data = np.vstack(([[-1, 0.04], [-0.8, 0.06]]*1000, Te_data))
+    Te_data = np.vstack((Te_data, [[0.8, 0.06], [1, 0.04]]*1000))
+    # Te_data = np.vstack(([[-1, 0.0]]*100, Te_data))
+    # Te_data = np.vstack((Te_data, [[1, 0.0]]*100))
 
     poptTe, pcovTe = optimize.curve_fit(TeFit, Te_data[:, 0], Te_data[:, 1],
                                         p0=coeffsTe0, maxfev=5000)

reconstruct_ne.py
@@ -90,14 +90,15 @@ def integrate_traj(tr, lam, get_rho, ne, coeffs, Te,
         sigmaEff_loc = (sigmaEff12(Te_loc) + sigmaEff13(Te_loc))
     else:
         # simple assumption for SOL
-        Te_loc = 0.04  # 0.
+        Te_loc = Te(1.0)  # 0.
         ne_loc = 1e19 * 1e-2  # 0.
         sigmaEff_loc = (sigmaEff12(Te_loc) + sigmaEff13(Te_loc))  # 0.
 
     # calculate total value with integrals
     # SV size, ion zones should be calculated!
     # lam = np.linalg.norm(tr.ion_zones[2][0] - tr.ion_zones[2][-1])  # [m]
-    Itot = 2 * ne_loc * sigmaEff_loc * lam(r_loc) * math.exp(-I1-I2)  # relative to I0
+    # Itot relative to I0
+    Itot = 2 * ne_loc * sigmaEff_loc * lam(r_loc) * math.exp(-I1-I2)
 
     return np.array([tr.Ebeam, tr.U['A2'], r_loc, Itot, ne_loc, Te_loc,
                      lam(r_loc), sigmaEff_loc, I1, I2, L1, L2])
@@ -337,47 +338,48 @@ sigmaV23_p = np.loadtxt(filename)  # [0] Te [eV] [1] <sigma*v> [cm^3/s]
 sigmaV23_p[:, 1] = sigmaV23_p[:, 1]*1e-6  # <sigma*v> goes to [m^3/s]
 
 # %% interpolate rates
-
+interp_type = 'quadratic'
+# interp_type = 'slinear'
 sigmaEff12_e_interp = interpolate.interp1d(sigmaV12_e[:, 0]/1e3,
                                            sigmaV12_e[:, 1]/v0,
-                                           kind='linear')  # Te in [keV]
+                                           kind=interp_type)  # Te in [keV]
 sigmaEff12_p_interp = interpolate.interp1d(sigmaV12_p[:, 0]/1e3,
                                            sigmaV12_p[:, 1]/v0,
-                                           kind='linear')  # Te in [keV]
+                                           kind=interp_type)  # Te in [keV]
 sigmaEff13_e_interp = interpolate.interp1d(sigmaV13_e[:, 0]/1e3,
                                            sigmaV13_e[:, 1]/v0,
-                                           kind='linear')  # Te in [keV]
+                                           kind=interp_type)  # Te in [keV]
 sigmaEff23_e_interp = interpolate.interp1d(sigmaV23_e[:, 0]/1e3,
                                            sigmaV23_e[:, 1]/v0,
-                                           kind='linear')  # Te in [keV]
+                                           kind=interp_type)  # Te in [keV]
 sigmaEff23_p_interp = interpolate.interp1d(sigmaV23_p[:, 0]/1e3,
                                            sigmaV23_p[:, 1]/v0,
-                                           kind='linear')  # Te in [keV]
+                                           kind=interp_type)  # Te in [keV]
 
 plt.figure()
 plt.semilogx(sigmaV12_e[:, 0], sigmaV12_e[:, 1]*1e6, 'o', color='k',
              label=r'$Cs^+$+e $\rightarrow$ $Cs^{2+}$+2e')
-Temp = np.linspace(min(sigmaV12_e[:, 0]), max(sigmaV12_e[:, 0]), num=1000)
+Temp = np.linspace(min(sigmaV12_e[:, 0]), max(sigmaV12_e[:, 0]), num=5000)
 plt.semilogx(Temp, sigmaEff12_e_interp(Temp/1e3)*1e6*v0, '-', color='k')
 
 plt.semilogx(sigmaV13_e[:, 0], sigmaV13_e[:, 1]*1e6, 'o', color='g',
              label=r'$Cs^+$+e $\rightarrow$ $Cs^{3+}$+3e')
-Temp = np.linspace(min(sigmaV13_e[:, 0]), max(sigmaV13_e[:, 0]), num=100)
+Temp = np.linspace(min(sigmaV13_e[:, 0]), max(sigmaV13_e[:, 0]), num=5000)
 plt.semilogx(Temp, sigmaEff13_e_interp(Temp/1e3)*1e6*v0, '-', color='g')
 
 plt.semilogx(sigmaV12_p[:, 0], sigmaV12_p[:, 1]*1e6, 'o', color='r',
              label=r'$Cs^+$+p $\rightarrow$ $Cs^{2+}$+p+e')
-Temp = np.linspace(min(sigmaV12_p[:, 0]), max(sigmaV12_p[:, 0]), num=100)
+Temp = np.linspace(min(sigmaV12_p[:, 0]), max(sigmaV12_p[:, 0]), num=5000)
 plt.semilogx(Temp, sigmaEff12_p_interp(Temp/1e3)*1e6*v0, '-', color='r')
 
 plt.semilogx(sigmaV23_e[:, 0], sigmaV23_e[:, 1]*1e6, '^', color='k',
              label=r'$Cs^{2+}$+e $\rightarrow$ $Cs^{3+}$+2e')
-Temp = np.linspace(min(sigmaV23_e[:, 0]), max(sigmaV23_e[:, 0]), num=100)
+Temp = np.linspace(min(sigmaV23_e[:, 0]), max(sigmaV23_e[:, 0]), num=5000)
 plt.semilogx(Temp, sigmaEff23_e_interp(Temp/1e3)*1e6*v0, '--', color='k')
 
 plt.semilogx(sigmaV23_p[:, 0], sigmaV23_p[:, 1]*1e6, 's', color='r',
              label=r'$Cs^{2+}$+p $\rightarrow$ $Cs^{3+}$+p+e')
-Temp = np.linspace(min(sigmaV23_p[:, 0]), max(sigmaV23_p[:, 0]), num=100)
+Temp = np.linspace(min(sigmaV23_p[:, 0]), max(sigmaV23_p[:, 0]), num=5000)
 plt.semilogx(Temp, sigmaEff23_p_interp(Temp/1e3)*1e6*v0, '--', color='r')
 
 plt.xlabel(r'$E_{e,p}$ (eV)')
@@ -414,17 +416,17 @@ Itot = np.delete(Itot, [2, 4, 6], axis=1)
 print('experimental Itot loaded ' + filename)
 
 neAvg = np.mean(Itot[:, 4])  # line averagend ne [e19 m-3]
-neAvg = 0.8  # 0.46  # 3.6
+neAvg = 0.77  # 0.46  # 3.6
 print('\n****** ne = {:.2f}\n'.format(neAvg))
 
-Iinj = 52e-6  # 100e-6  # injection beam current [A]
+Iinj = 45e-6  #52e-6  # 100e-6  # injection beam current [A]
 kAmpl = 1 * 1e7  # amplification coefficient
 
 # flag to optimize I0
 optimizeI0 = False
 
 # flag to optimize ne
-optimizeNe = True
+optimizeNe = False
 
 # flag to plot parameters vs rho
 addPlots = False